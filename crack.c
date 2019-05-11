3e865053732a46d934f1a32ca610d5ea658f1e1d
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-11 17:00:57 +1000

    Test4

--- crack.c
+++ crack.c
@@ -10,13 +10,14 @@ void readFourLetterPasswords(char* passwords[]){
     fp = fopen("pwd4sha256", "r");
 
     for(int i=0;i<NUM_FOUR_LETTER;i++){
-        const unsigned char* buff = (unsigned char*)malloc(sizeof(char)*33);
+        unsigned char* buff = (unsigned char*)malloc(sizeof(char)*33);
         char dump[65];
-        fread(buff, 32, 1 , fp);
+        fread(buff, 32, 1, fp);
         for(int j=0;j<32;j++){
             sprintf(dump+(j*2), "%02x", buff[j]);
         }
         strcpy(passwords[i], dump);
+        fp+=32;
     }
 
 
