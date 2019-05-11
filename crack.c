8eb91bfe88d3dfd46edd5b36305f7877946cd3a8
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-11 15:59:00 +1000

    Added main

---
+++ crack.c
@@ -0,0 +1,20 @@
+
+void readFourLetterPasswords(char* dump){
+    FILE* fp;
+    fp = fopen("pwd4sha256", 'r');
+    const unsigned char* buff = (unsigned char*)malloc(sizeof(char)*33);
+    fread(buff, 32, 1 , fp);
+    for(int i=0;i<32;i++){
+        sprintf(dump+(i*2), "%02x", buff[i]);
+        printf("%02x" buff[i]);
+    }
+}
+
+
+int main(int argc, char* argv[]){
+
+    char dump[65];
+    readFourLetterPasswords(dump);
+
+    return 0;
+}
