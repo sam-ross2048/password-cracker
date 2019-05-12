5581a6a7a5ffe35c42732665cc3472d9baa9fb6c
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-13 09:59:50 +1000

    Test guess passwords

--- crack.c
+++ crack.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "sha256.h"
 
 #define NUM_FOUR_LETTER 10
 
@@ -28,10 +29,25 @@ void readFourLetterPasswords(char** passwords){
 }
 
 
+void guessPasswords(char** passwords){
+    char* guess = "samr";
+    SHA256_CTX* data = (SHA256_CTX*)malloc(sizeof(SHA256_CTX));
+    assert(data);
+    sha256_init(data);
+    sha256_update(data, guess);
+    for(int i=0;i<NUM_FOUR_LETTER;i++){
+        if(strcmp(passwords[i], data->data)==0){
+            printf("MATCH!\n");
+        }
+
+    }
+
+}
+
 int main(int argc, char* argv[]){
 
     char* passwords[NUM_FOUR_LETTER];
     readFourLetterPasswords(passwords);
-    //printf("%s\n", dump);
+    guessPasswords(passwords);
     return 0;
 }
