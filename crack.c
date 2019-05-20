30f778c04159933d46c18053b0b536a8b6b1a563
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-20 14:10:18 +1000

    Test8

--- crack.c
+++ crack.c
@@ -71,7 +71,6 @@ void readPasswords(char* passwords[], char* filename){
         readSinglePassword(dump, fp);
         passwords[i] = (char*)malloc(sizeof(char)*HASH_SIZE);
         strcpy(passwords[i], dump);
-        printf("%s\n", passwords[i]);
     }
 }
 
