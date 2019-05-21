dcb157636fe179cef6f4dbeb24b0ba4d061a7ccb
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-21 19:47:16 +1000

    Testing2

--- crack.c
+++ crack.c
@@ -267,7 +267,9 @@ static void bruteForce(int maxlen, int alphaLen, int alphaOffset){
 
 char* zeroPad(int number, int numDigits){
     char* guess = (char*)malloc(sizeof(char)*(numDigits+1));
-	sprintf(guess, "%0dd", numDigits, number);
+	char* format = "%0";
+	sprintf(format, "d", numDigits);
+	sprintf(guess, format, number);
     return guess;
 }
 
