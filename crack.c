f29dd29b12556b542a04c1c8215064ba92114063
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-21 22:50:01 +1000

    Test14

--- crack.c
+++ crack.c
@@ -362,6 +362,7 @@ void fileGeneration(char* filename, int length, int maxGuesses, int* numGuesses)
 	char word[length+1];
 	FILE* fp = fopen(filename, "r");
 	while(/*readFilePassword(fp, word, length)!=true && */*numGuesses < maxGuesses){
+		printf("NUMGUESSES: %d  MAX: %d", *numGuesses, maxGuesses);
 		word[length] = '\0';
 		printf("%s\n", word);
 		*numGuesses++;
