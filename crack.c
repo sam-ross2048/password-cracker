d1d8a15c71ae45860578721c57044f75d850f916
sam-ross2048 <xtlxtl12@gmail.com>
2019-05-21 22:53:36 +1000

    Test15

--- crack.c
+++ crack.c
@@ -373,8 +373,8 @@ void fileGeneration(char* filename, int length, int maxGuesses, int* numGuesses)
 
 
 void generateGuesses(int maxGuesses, int length){
-	int* numGuesses; int n = 0;
-	numGuesses=&n;
+	int n = 0;
+	int* numGuesses=&n;
 	fileGeneration(COMMON_FILE, length, maxGuesses, numGuesses);
 }
 
